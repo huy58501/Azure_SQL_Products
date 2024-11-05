68b43e300eb2c47756a07080dddfd2624e719f06
Tony Nguyen <tonynguyen@Tonys-Mac-mini.local>
2024-11-05 15:36:55 -0500

    modified server.js

--- server.js
+++ server.js
@@ -47,5 +47,5 @@ app.get('/', (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at ${port}`);
 });
